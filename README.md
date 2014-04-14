3b0845e2abf866b7c468b8aee6a9ebcaf2c10116
Frug <frugen@gmail.com>
2014-04-14 14:25:48 -0400

    comments, readme update

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-js-bbcode-parser
-================
+JavaScript BBCode Parser
+========================
 
-Simple, extendible javascript BBCode parser.
+This is a simple JavaScript BBCode parser module that returns html when fed BBCodes.
+
+It's designed to be light-weight and allows you to easily add your own BBCodes with only basic JavaScript knowledge.
+
+It was based initially on the Extendible BBCode Parser by Patrick Gillespie ( github @patorjk )
+with help from the regexp [blogs of Steven Levithan](http://blog.stevenlevithan.com/) ( github @slevithan )
+
+
+Usage
+-----
+Include your configuration file, which defines all the BBCodes you want to support, and all the valid color codes.
+You can do this in your document head or at the bottom of your page.
+`<script src="bbcode-config.js"></script>`
+Below that, include the parser itself.
+`<script src="bbcode-parser.js"></script>`
+
+Call the parser with the following method
+`var output = BBCodeParser.process("Your string");`
+
+
+Configuration
+-------------
+
+
+License
+-------
+This project is released under The MIT License (MIT).
--- bbcode-parser.js
+++ bbcode-parser.js
@@ -48,6 +48,16 @@ var BBCodeParser = (function(parserTags, parserColors) {
 	
 	regExpAllowedColors = new RegExp('^(?:' + parserColors.join('|') + ')$');
 	
+	/* 
+	 * Create a regular expression that captures the innermost instance of a tag in an array of tags
+	 * The returned RegExp captures the following in order:
+	 * 1) the tag from the array that was matched
+	 * 2) all (optional) parameters included in the opening tag
+	 * 3) the contents surrounded by the tag
+	 * 
+	 * @param {type} tagsArray - the array of tags to capture
+	 * @returns {RegExp}
+	 */
 	function createInnermostTagRegExp(tagsArray) {
 		var openingTag = '\\[(' + tagsArray.join('|') + ')\\b(?:[ =]([\\w"#\\-\\:\\/= ]*?))?\\]',
 			notContainingOpeningTag = '((?:(?=([^\\[]+))\\4|\\[(?!\\1\\b(?:[ =](?:[\\w"#\\-\\:\\/= ]*?))?\\]))*?)',
@@ -56,6 +66,17 @@ var BBCodeParser = (function(parserTags, parserColors) {
 		return new RegExp( openingTag + notContainingOpeningTag + closingTag, 'i');
 	}
 	
+	/*
+	 * Escape the contents of a tag and mark the tag with a null unicode character.
+	 * To be used in a loop with a regular expression that captures tags.
+	 * Marking the tag prevents it from being matched again.
+	 * 
+	 * @param {type} matchStr - the full match, including the opening and closing tags
+	 * @param {type} tagName - the tag that was matched
+	 * @param {type} tagParams - parameters passed to the tag
+	 * @param {type} tagContents - everything between the opening and closing tags
+	 * @returns {String} - the full match with the tag contents escaped and the tag marked with \u0000
+	 */
 	function escapeInnerTags(matchStr, tagName, tagParams, tagContents) {
 		tagParams = tagParams || "";
 		tagContents = tagContents || "";
@@ -63,21 +84,33 @@ var BBCodeParser = (function(parserTags, parserColors) {
 		return "[\u0000" + tagName + tagParams + "]" + tagContents + "[/\u0000" + tagName + "]";
 	}
 	
+	/* 
+	 * Escape all BBCodes that are inside the given tags.
+	 * 
+	 * @param {string} text - the text to search through
+	 * @param {string[]} tags - the tags to search for
+	 * @returns {string} - the full text with the required code escaped
+	 */
 	function escapeBBCodesInsideTags(text, tags) {
 		var innerMostRegExp;
-		
 		if (tags.length === 0 || text.length < 7)
 			return text;
-
 		innerMostRegExp = createInnermostTagRegExp(tags);
-		
 		while (
 			text !== (text = text.replace(innerMostRegExp, escapeInnerTags))
 		);
-
 		return text.replace(/\u0000/g,'');
 	}
 	
+	/*
+	 * Process a tag and its contents according to the rules provided in parserTags.
+	 * 
+	 * @param {type} matchStr - the full match, including the opening and closing tags
+	 * @param {type} tagName - the tag that was matched
+	 * @param {type} tagParams - parameters passed to the tag
+	 * @param {type} tagContents - everything between the opening and closing tags
+	 * @returns {string} - the fully processed tag and its contents
+	 */
 	function replaceTagsAndContent(matchStr, tagName, tagParams, tagContents) {
 		tagName = tagName.toLowerCase();
 		tagParams = tagParams || "";
@@ -85,13 +118,13 @@ var BBCodeParser = (function(parserTags, parserColors) {
 		return parserTags[tagName].openTag(tagParams, tagContents) + (parserTags[tagName].content ? parserTags[tagName].content(tagParams, tagContents) : tagContents) + parserTags[tagName].closeTag(tagParams, tagContents);
 	}
 	
-	function processTags(text, tags) {
+	function processTags(text, tagNames) {
 		var innerMostRegExp;
 		
-		if (tags.length === 0 || text.length < 7)
+		if (tagNames.length === 0 || text.length < 7)
 			return text;
 		
-		innerMostRegExp = createInnermostTagRegExp(tags);
+		innerMostRegExp = createInnermostTagRegExp(tagNames);
 		
 		while (
 			text !== (text = text.replace(innerMostRegExp, replaceTagsAndContent))
@@ -100,12 +133,13 @@ var BBCodeParser = (function(parserTags, parserColors) {
 		return text;
 	}
 	
+	/*
+	 * Public Methods and Properties
+	 */
 	me.process = function(text, config) {
 		text = escapeBBCodesInsideTags(text, tagNamesNoParse);
 		
-		text = processTags(text, tagNames);
-		
-		return text;
+		return processTags(text, tagNames);
 	};
 	
 	me.allowedTags = tagNames;
